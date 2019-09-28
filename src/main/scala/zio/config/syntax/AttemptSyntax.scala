6579733134173c7b86e2fdf011c265e2ff2ea3c4
wiem <wiemzin@gmail.com>
2019-09-28 23:15:26 +0200

    fixed error

--- src/main/scala/zio/config/syntax/AttemptSyntax.scala
+++ src/main/scala/zio/config/syntax/AttemptSyntax.scala
@@ -1,11 +1,14 @@
 package zio.config.syntax
 
-import scala.util.Try
+import scala.util.{ Failure, Success, Try }
 
 trait AttemptSyntax {
   implicit class ConfigSyntaxSyntaxOps[A](self: => A) {
 
     def attempt[E](f: Throwable => E): Either[E, A] =
-      Try(self).fold(e => Left(f(e)), Right(_))
+      Try(self) match {
+        case Success(value)     => Right(value)
+        case Failure(throwable) => Left(f(throwable))
+      }
   }
 }
