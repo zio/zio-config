bfd97bd4fe0ac9a63f875ec3a8b8d246ba0ebf53
afsalthaj <afsal.taj06@gmail.com>
2019-10-10 22:35:25 +1100

    Remove the node from README

--- zio-config/src/main/scala/zio/config/ReadError.scala
+++ zio-config/src/main/scala/zio/config/ReadError.scala
@@ -7,6 +7,5 @@ sealed trait ReadError[+K, +V] {
 object ReadError {
   final case class MissingValue[K](key: K)                               extends ReadError[K, Nothing]
   final case class ParseError[K, V](key: K, provided: V, `type`: String) extends ReadError[K, V]
-  final case class FoundNestedObject[K](key: K)                          extends ReadError[K, Nothing]
   final case class FatalError[K](key: K, cause: Throwable)               extends ReadError[K, Nothing]
 }
