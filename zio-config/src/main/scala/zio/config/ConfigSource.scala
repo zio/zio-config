42918123f5ab2266353f82a819405f23596d29a3
Michael Thomas <Michaelt293@gmail.com>
2019-10-12 07:48:49 +1100

    Added methods for combining config sources

--- zio-config/src/main/scala/zio/config/ConfigSource.scala
+++ zio-config/src/main/scala/zio/config/ConfigSource.scala
@@ -10,4 +10,37 @@ object ConfigSource {
   trait Service {
     def getConfigValue(path: String): IO[ReadError, String]
   }
+
+  implicit class ConfigSourceOps(private val source: ConfigSource) extends AnyVal {
+    def orElse(that: => ConfigSource): ConfigSource =
+      new ConfigSource {
+        val configService: ConfigSource.Service =
+          (path: String) =>
+            source.configService
+              .getConfigValue(path)
+              .orElse(
+                that.configService
+                  .getConfigValue(path)
+              )
+      }
+
+    def catchSome(pf: PartialFunction[ReadError, ConfigSource]): ConfigSource =
+      new ConfigSource {
+        val configService: ConfigSource.Service =
+          (path: String) =>
+            source.configService
+              .getConfigValue(path)
+              .catchSome {
+                pf.andThen(
+                  _.configService
+                    .getConfigValue(path)
+                )
+              }
+      }
+
+    def ifMissingValue(that: => ConfigSource): ConfigSource =
+      catchSome {
+        case ReadError.MissingValue(_) => that
+      }
+  }
 }
