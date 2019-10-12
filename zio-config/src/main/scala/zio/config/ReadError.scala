aecfb09126610918e08b6fe9f6f556d197830a52
Michael Thomas <Michaelt293@users.noreply.github.com>
2019-10-13 10:58:00 +1100

    Extend error types with NoStackTrace (#84)
    * Experimenting with extending error types with Exception
    
    * Fix imports
    
    * Extend with NoStackTrace
    
    * Deleted Example.scala

--- zio-config/src/main/scala/zio/config/ReadError.scala
+++ zio-config/src/main/scala/zio/config/ReadError.scala
@@ -1,7 +1,19 @@
 package zio.config
 
-sealed trait ReadError {
+import scala.util.control.NoStackTrace
+
+sealed trait ReadError extends NoStackTrace {
   val key: String
+}
+
+final case class ReadErrors(errors: ::[ReadError]) extends NoStackTrace
+
+object ReadErrors {
+  def apply(a: ReadError, as: ReadError*): ReadErrors =
+    ReadErrors(::(a, as.toList))
+
+  def concat(l: ReadErrors, r: ReadErrors): ReadErrors =
+    ReadErrors(::(l.errors.head, l.errors.tail ++ r.errors))
 }
 
 object ReadError {
