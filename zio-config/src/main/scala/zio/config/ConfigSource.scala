481437788e6e16b8a4799a197dae928e99262bf2
Michael Thomas <Michaelt293@gmail.com>
2019-10-12 17:09:49 +1100

    Remove implicit class and add methods to ConfigSource

--- zio-config/src/main/scala/zio/config/ConfigSource.scala
+++ zio-config/src/main/scala/zio/config/ConfigSource.scala
@@ -2,8 +2,62 @@ package zio.config
 
 import zio.IO
 
-trait ConfigSource {
+trait ConfigSource { self =>
   val configService: ConfigSource.Service
+
+  def orElse(that: ConfigSource): ConfigSource =
+    new ConfigSource {
+      val configService: ConfigSource.Service =
+        new ConfigSource.Service {
+          val sourceDescription: String = {
+            val selfDesc = self.configService.sourceDescription
+            val thatDesc = that.configService.sourceDescription
+
+            s"${selfDesc}: with fallback source: ${thatDesc} (on all errors)"
+          }
+
+          def getConfigValue(path: String): IO[ReadError, String] =
+            self.configService
+              .getConfigValue(path)
+              .orElse(
+                that.configService
+                  .getConfigValue(path)
+              )
+        }
+    }
+
+  def catchSome(
+    pf: PartialFunction[ReadError, ConfigSource],
+    fallbackSource: String,
+    fallbackDescription: String
+  ): ConfigSource =
+    new ConfigSource {
+      val configService: ConfigSource.Service =
+        new ConfigSource.Service {
+          val sourceDescription: String = {
+            val selfDesc = self.configService.sourceDescription
+
+            s"${selfDesc}: with fallback source: ${fallbackSource} (${fallbackDescription})"
+          }
+
+          def getConfigValue(path: String): IO[ReadError, String] =
+            self.configService
+              .getConfigValue(path)
+              .catchSome {
+                pf.andThen(
+                  _.configService
+                    .getConfigValue(path)
+                )
+              }
+        }
+    }
+
+  def ifMissingValue(that: => ConfigSource): ConfigSource =
+    catchSome(
+      { case ReadError.MissingValue(_) => that },
+      that.configService.sourceDescription,
+      "on missing value"
+    )
 }
 
 object ConfigSource {
@@ -11,61 +65,5 @@ object ConfigSource {
     val sourceDescription: String
 
     def getConfigValue(path: String): IO[ReadError, String]
-  }
-
-  implicit class ConfigSourceOps(private val source: ConfigSource) extends AnyVal {
-    def orElse(that: ConfigSource): ConfigSource =
-      new ConfigSource {
-        val configService: ConfigSource.Service =
-          new Service {
-            val sourceDescription: String = {
-              val sourceDesc = source.configService.sourceDescription
-              val thatDesc   = that.configService.sourceDescription
-
-              s"${sourceDesc}: with fallback source: ${thatDesc} (on all errors)"
-            }
-
-            def getConfigValue(path: String): IO[ReadError, String] =
-              source.configService
-                .getConfigValue(path)
-                .orElse(
-                  that.configService
-                    .getConfigValue(path)
-                )
-          }
-      }
-
-    def catchSome(
-      pf: PartialFunction[ReadError, ConfigSource],
-      fallbackSource: String,
-      fallbackDescription: String
-    ): ConfigSource =
-      new ConfigSource {
-        val configService: ConfigSource.Service =
-          new Service {
-            val sourceDescription: String = {
-              val sourceDesc = source.configService.sourceDescription
-
-              s"${sourceDesc}: with fallback source: ${fallbackSource} (${fallbackDescription})"
-            }
-
-            def getConfigValue(path: String): IO[ReadError, String] =
-              source.configService
-                .getConfigValue(path)
-                .catchSome {
-                  pf.andThen(
-                    _.configService
-                      .getConfigValue(path)
-                  )
-                }
-          }
-      }
-
-    def ifMissingValue(that: => ConfigSource): ConfigSource =
-      catchSome(
-        { case ReadError.MissingValue(_) => that },
-        that.configService.sourceDescription,
-        "on missing value"
-      )
   }
 }
