96815db61ae933545019c48376cb020351667587
afsalthaj <afsal.taj06@gmail.com>
2019-12-02 23:55:02 +1100

    Support nested class as nested config

--- examples/src/main/scala/zio/config/examples/AutomaticConfigDescExample.scala
+++ examples/src/main/scala/zio/config/examples/AutomaticConfigDescExample.scala
@@ -5,14 +5,15 @@ import zio.config.ConfigSource
 import zio.config.magnolia.ConfigDescriptorProvider._
 import zio.console.Console.Live.console._
 
-final case class Aws(region: String)
+final case class Token(value: String) extends AnyVal
+final case class Aws(region: String, token: Token)
 final case class MyConfig(
+  aws: Aws,
   port: Int,
   dburl: String,
   port2: Option[Double],
   price: Either[Double, String],
   price2: Either[Double, String],
-  aws: Aws,
   default: Int = 1,
   anotherDefault: Int = 2
 )
@@ -24,12 +25,13 @@ object AutomaticConfigDescriptor extends zio.App {
   private val source =
     ConfigSource.fromMap(
       Map(
+        "aws.region"     -> "us-east",
+        "aws.token"      -> "some token",
         "port"           -> "1",
         "dburl"          -> "some url",
         "port2"          -> "3.14",
         "price"          -> "30 euros",
         "price2"         -> "50",
-        "region"         -> "us-east",
         "anotherDefault" -> "3"
       )
     )
@@ -41,10 +43,10 @@ object AutomaticConfigDescriptor extends zio.App {
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
     read(config).foldM(
       r => putStrLn(r.mkString(",")) *> ZIO.succeed(1),
-      result => putStrLn(result.toString) *> ZIO.succeed(0)
+      result => putStrLn(result.toString()) *> putStrLn(write(config, result).toString()) *> ZIO.succeed(0)
     )
   //
-  // MyConfig(1,some url,Some(3.14),Right(30 euros),Left(50.0),Aws(us-east),1,3)
+  // MyConfig(Aws(us-east,Token(some token)),1,some url,Some(3.14),Right(30 euros),Left(50.0),1,3)
   //
   // Process finished with exit code 0
   //
--- zio-config-magnolia/src/main/scala/zio/config/magnolia/ConfigDescriptorProvider.scala
+++ zio-config-magnolia/src/main/scala/zio/config/magnolia/ConfigDescriptorProvider.scala
@@ -7,7 +7,7 @@ import magnolia._
 import scala.language.experimental.macros
 
 trait ConfigDescriptorProvider[T] {
-  def getDescription(path: String): ConfigDescriptor[String, String, T]
+  def getDescription(path: Vector[String]): ConfigDescriptor[String, String, T]
 }
 
 object ConfigDescriptorProvider {
@@ -15,7 +15,30 @@ object ConfigDescriptorProvider {
 
   def instance[T](f: String => ConfigDescriptor[String, String, T]): ConfigDescriptorProvider[T] =
     new ConfigDescriptorProvider[T] {
-      override def getDescription(path: String): ConfigDescriptor[String, String, T] = f(path)
+      override def getDescription(path: Vector[String]): ConfigDescriptor[String, String, T] = {
+        def loop(
+          tail: List[String],
+          acc: ConfigDescriptor[String, String, T]
+        ): ConfigDescriptor[String, String, T] =
+          tail match {
+            case Nil =>
+              acc
+
+            case h :: Nil =>
+              loop(Nil, f(h))
+
+            case h :: tail =>
+              nested(h)(loop(tail, acc))
+          }
+
+        loop(
+          path.toList,
+          empty[String, String, T].xmapEither[T]({
+            case Some(v) => Right(v)
+            case None    => Left("failed - undefined state")
+          })(v => Right(Some(v)))
+        )
+      }
     }
 
   implicit val stringDescriptor: ConfigDescriptorProvider[String] =
@@ -28,31 +51,35 @@ object ConfigDescriptorProvider {
     instance(int)
 
   implicit def opt[A: ConfigDescriptorProvider]: ConfigDescriptorProvider[Option[A]] =
-    instance(path => ConfigDescriptorProvider[A].getDescription(path).optional)
+    ConfigDescriptorProvider[A].getDescription(_).optional
 
   // For automatic derivation, `Either` type in a case class implies, for the same label, it tries two descriptions and return the successful one as an Either.
   // This is equivalent to saying string("PATH").orElseEither(int("PATH")). During automatic derivations, we are unaware of alternate paths.
   implicit def eith[A: ConfigDescriptorProvider, B: ConfigDescriptorProvider]: ConfigDescriptorProvider[Either[A, B]] =
-    instance(
-      path =>
+    new ConfigDescriptorProvider[Either[A, B]] {
+      override def getDescription(path: Vector[String]): ConfigDescriptor[String, String, Either[A, B]] =
         ConfigDescriptorProvider[A].getDescription(path).orElseEither(ConfigDescriptorProvider[B].getDescription(path))
-    )
+    }
 
   type Typeclass[T] = ConfigDescriptorProvider[T]
 
   def combine[T](caseClass: CaseClass[ConfigDescriptorProvider, T]): ConfigDescriptorProvider[T] =
     new ConfigDescriptorProvider[T] {
-      def getDescription(path: String): ConfigDescriptor[String, String, T] = {
+      def getDescription(path: Vector[String]): ConfigDescriptor[String, String, T] = {
         // Loop instead of map to do more things.
-        def loop(seq: Seq[Param[ConfigDescriptorProvider, T]]): List[ConfigDescriptor[String, String, Any]] =
+        def loop(
+          seq: Seq[Param[ConfigDescriptorProvider, T]]
+        ): List[ConfigDescriptor[String, String, Any]] =
           seq.toList match {
             case Nil => Nil
             case h :: t =>
               loop(t) :+ {
+                val derivedPath = if (caseClass.isValueClass) path else path :+ h.label
+                val rawDesc     = h.typeclass.getDescription(derivedPath)
                 val desc =
                   h.default
-                    .map(r => h.typeclass.getDescription(h.label).default(r))
-                    .getOrElse(h.typeclass.getDescription(h.label))
+                    .map(r => rawDesc.default(r))
+                    .getOrElse(rawDesc)
 
                 desc.xmap(r => r: Any)(r => r.asInstanceOf[h.PType])
               }
@@ -61,6 +88,7 @@ object ConfigDescriptorProvider {
         collectAll(loop(caseClass.parameters)).xmap[T](
           caseClass.rawConstruct
         )(v => caseClass.parameters.map(_.dereference(v): Any).toList)
+
       }
     }
 
@@ -69,5 +97,5 @@ object ConfigDescriptorProvider {
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 
   def description[T: ConfigDescriptorProvider]: ConfigDescriptor[String, String, T] =
-    ConfigDescriptorProvider[T].getDescription("")
+    ConfigDescriptorProvider[T].getDescription(Vector.empty)
 }
