e8a6444e7ccc8f301fd80c5aa92aa3fbc066d671
leigh-perry <lperry.breakpoint@gmail.com>
2019-12-06 05:26:23 +0000

    Tests for StringSupport

--- zio-config-refined/src/test/scala/zio/config/refined/StringSupportTest.scala
+++ zio-config-refined/src/test/scala/zio/config/refined/StringSupportTest.scala
@@ -1,8 +1,10 @@
 package zio.config.refined
 
+import java.util.UUID
+
 import eu.timepit.refined.W
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.string.{ EndsWith, IPv4 }
+import eu.timepit.refined.string.{ EndsWith, IPv4, IPv6, MatchesRegex, Regex, Uri, Url, Uuid }
 import zio.ZIO
 import zio.config.ConfigDescriptor._
 import zio.config.helpers._
@@ -50,6 +52,102 @@ object StringSupportTest
           checkM(Gen.listOfN(4)(Gen.int(256, 1000)).map(_.mkString("."))) { p =>
             val cfg = iPv4(string("TEST"))
             val p2: ZIO[Any, ReadErrorsVector[String, String], Refined[String, IPv4]] =
+              read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
+
+            assertM(p2.either, helpers.assertErrors(_.size == 1))
+          }
+        },
+        testM("Refined config IPv6 invalid") {
+          checkM(genSymbol(0, 10).map(s => s + "ab")) { p =>
+            val cfg = iPv6(string("TEST"))
+            val p2: ZIO[Any, ReadErrorsVector[String, String], Refined[String, IPv6]] =
+              read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
+
+            assertM(p2.either, helpers.assertErrors(_.size == 1))
+          }
+        },
+        testM("Refined config MatchesRegex roundtrip") {
+          checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, MatchesRegex[W.`".*abc"`.T]](s + "abc"))) { p =>
+            val cfg = matchesRegex[W.`".*abc"`.T](string("TEST"))
+            val p2 =
+              for {
+                written <- ZIO.fromEither(write(cfg, p))
+                reread  <- read(cfg from ConfigSource.fromPropertyTree(written))
+              } yield reread
+
+            assertM(p2, equalTo(p))
+          }
+        },
+        testM("Refined config MatchesRegex invalid") {
+          checkM(genSymbol(0, 10).map(s => s + "ab")) { p =>
+            val cfg = matchesRegex[W.`".*abc"`.T](string("TEST"))
+            val p2: ZIO[Any, ReadErrorsVector[String, String], Refined[String, MatchesRegex[W.`".*abc"`.T]]] =
+              read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
+
+            assertM(p2.either, helpers.assertErrors(_.size == 1))
+          }
+        },
+        testM("Refined config Regex roundtrip") {
+          checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, Regex](s + ".+abc"))) { p =>
+            val cfg = regex(string("TEST"))
+            val p2 =
+              for {
+                written <- ZIO.fromEither(write(cfg, p))
+                reread  <- read(cfg from ConfigSource.fromPropertyTree(written))
+              } yield reread
+
+            assertM(p2, equalTo(p))
+          }
+        },
+        testM("Refined config Regex invalid") {
+          checkM(genSymbol(0, 10).map(s => s + "\\q5ab")) { p =>
+            val cfg = regex(string("TEST"))
+            val p2: ZIO[Any, ReadErrorsVector[String, String], Refined[String, Regex]] =
+              read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
+
+            assertM(p2.either, helpers.assertErrors(_.size == 1))
+          }
+        },
+        testM("Refined config Uri roundtrip") {
+          checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, Uri]("https://" + s + "w.asdf.asdf"))) { p =>
+            val cfg = uri(string("TEST"))
+            val p2 =
+              for {
+                written <- ZIO.fromEither(write(cfg, p))
+                reread  <- read(cfg from ConfigSource.fromPropertyTree(written))
+              } yield reread
+
+            assertM(p2, equalTo(p))
+          }
+        },
+        testM("Refined config Url roundtrip") {
+          checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, Url]("https://" + s + "w.asdf.asdf"))) { p =>
+            val cfg = url(string("TEST"))
+            val p2 =
+              for {
+                written <- ZIO.fromEither(write(cfg, p))
+                reread  <- read(cfg from ConfigSource.fromPropertyTree(written))
+              } yield reread
+
+            assertM(p2, equalTo(p))
+          }
+        },
+        testM("Refined config Uuid roundtrip") {
+          checkM(Gen.fromEffect(ZIO.succeed(Refined.unsafeApply[String, Uuid](UUID.randomUUID().toString)))) { p =>
+            val cfg = uuid(string("TEST"))
+            val p2 =
+              for {
+                written <- ZIO.fromEither(write(cfg, p))
+                reread  <- read(cfg from ConfigSource.fromPropertyTree(written))
+              } yield reread
+
+            assertM(p2, equalTo(p))
+          }
+        },
+        testM("Refined config Uuid invalid") {
+          checkM(Gen.fromEffect(ZIO.succeed(UUID.randomUUID().toString + "ab"))) { p =>
+            val cfg = uuid(string("TEST"))
+            val p2: ZIO[Any, ReadErrorsVector[String, String], Refined[String, Uuid]] =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
             assertM(p2.either, helpers.assertErrors(_.size == 1))
