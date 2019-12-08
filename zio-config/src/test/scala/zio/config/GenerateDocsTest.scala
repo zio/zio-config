5197ccce0a1d641d35cd2d6a9d23c7bd0c26d0fa
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2019-12-08 20:22:55 +0100

    Use map as a source

--- zio-config/src/test/scala/zio/config/GenerateDocsTest.scala
+++ zio-config/src/test/scala/zio/config/GenerateDocsTest.scala
@@ -1,8 +1,9 @@
 package zio.config
 
-import zio.config.ConfigDescriptor.{ int, string }
+import zio.config.ConfigDescriptor.{ int, nested, string }
 import zio.config.ConfigDocs.Details.{ Descriptions, DescriptionsWithValue }
-import zio.config.ConfigDocs.{ Both, Path }
+import zio.config.ConfigDocs.{ Both, NestedPath, Path }
+import zio.config.ConfigSource.{ ConstantMap, EmptySource }
 import zio.config.GenerateDocsTestUtils._
 import zio.config.helpers._
 import zio.random.Random
@@ -14,12 +15,12 @@ object GenerateDocsTest
       suite("Generate docs")(
         testM("generate docs") {
           check(generateDocsParams) { p =>
-            assert(generateDocs(p.config), equalTo(p.docs))
+            assert(generateDocs(p.descriptor from p.source), equalTo(p.docs))
           }
         },
         testM("generate docs with value") {
           check(generateDocsParams) { p =>
-            assert(generateDocsWithValue(p.config, p.appConfig), equalTo(p.docsWithValue))
+            assert(generateDocsWithValue(p.descriptor from p.source, p.value), equalTo(p.docsWithValue))
           }
         }
       )
@@ -29,40 +30,85 @@ object GenerateDocsTestUtils {
   final case class Credentials(user: String, password: String)
   final case class Database(port: Int, url: String)
   final case class AppConfig(secret: Option[String], credentials: Credentials, database: Database)
-  final case class GenerateDocsParams(appConfig: AppConfig) {
 
-    def config: ConfigDescriptor[String, String, AppConfig] = {
-      val credentials = (string("USER") ? "Example: ZioUser" |@| string("PASSWORD") ? "Example: ZioPass")(
+  final case class KeyParams(
+    secret: String,
+    credentials: String,
+    user: String,
+    password: String,
+    database: String,
+    url: String,
+    port: String
+  )
+  final case class GenerateDocsParams(keys: KeyParams, value: AppConfig) {
+
+    def descriptor: ConfigDescriptor[String, String, AppConfig] = {
+      val credentials = (string(keys.user) ? "Example: ZioUser" |@| string(keys.password) ? "Example: ZioPass")(
         Credentials.apply,
         Credentials.unapply
       ) ? "Credentials"
 
-      val database = (int("PORT") ? "Example: 8088" |@| string("URL") ? "Example: abc.com")(
+      val database = (int(keys.port) ? "Example: 8088" |@| string(keys.url) ? "Example: abc.com")(
         Database.apply,
         Database.unapply
       ) ? "Database"
 
-      (string("APP_SECRET").optional ? "Application secret" |@| credentials |@| database)(
+      (string(keys.secret).optional ? "Application secret" |@| nested(keys.credentials)(credentials) |@| nested(
+        keys.database
+      )(database))(
         AppConfig.apply,
         AppConfig.unapply
       )
     }
 
+    def source: ConfigSource[String, String] =
+      ConfigSource.fromMap(
+        Seq(
+          value.secret.map(keys.secret -> _),
+          Seq(
+            s"${keys.credentials}.${keys.user}"     -> value.credentials.user,
+            s"${keys.credentials}.${keys.password}" -> value.credentials.password
+          ),
+          Seq(
+            s"${keys.database}.${keys.url}"  -> value.database.url,
+            s"${keys.database}.${keys.port}" -> value.database.port.toString
+          )
+        ).flatten.toMap
+      )
+
     def docs: ConfigDocs[String, String] =
       Both(
         Both(
           Path(
-            "APP_SECRET",
-            Descriptions(List("<empty>", "value of type string", "optional value", "Application secret"))
+            keys.secret,
+            Descriptions(List(EmptySource, ConstantMap, "value of type string", "optional value", "Application secret"))
           ),
-          Both(
-            Path("USER", Descriptions(List("<empty>", "value of type string", "Example: ZioUser", "Credentials"))),
-            Path("PASSWORD", Descriptions(List("<empty>", "value of type string", "Example: ZioPass", "Credentials")))
+          NestedPath(
+            keys.credentials,
+            Both(
+              Path(
+                keys.user,
+                Descriptions(List(EmptySource, ConstantMap, "value of type string", "Example: ZioUser", "Credentials"))
+              ),
+              Path(
+                keys.password,
+                Descriptions(List(EmptySource, ConstantMap, "value of type string", "Example: ZioPass", "Credentials"))
+              )
+            )
           )
         ),
-        Both(
-          Path("PORT", Descriptions(List("<empty>", "value of type int", "Example: 8088", "Database"))),
-          Path("URL", Descriptions(List("<empty>", "value of type string", "Example: abc.com", "Database")))
+        NestedPath(
+          keys.database,
+          Both(
+            Path(
+              keys.port,
+              Descriptions(List(EmptySource, ConstantMap, "value of type int", "Example: 8088", "Database"))
+            ),
+            Path(
+              keys.url,
+              Descriptions(List(EmptySource, ConstantMap, "value of type string", "Example: abc.com", "Database"))
+            )
+          )
         )
       )
 
@@ -71,42 +117,48 @@ object GenerateDocsTestUtils {
         Both(
           Both(
             Path(
-              "APP_SECRET",
+              keys.secret,
               DescriptionsWithValue(
-                appConfig.secret,
-                List("<empty>", "value of type string", "optional value", "Application secret")
+                value.secret,
+                List(EmptySource, ConstantMap, "value of type string", "optional value", "Application secret")
               )
             ),
-            Both(
-              Path(
-                "USER",
-                DescriptionsWithValue(
-                  Some(appConfig.credentials.user),
-                  List("<empty>", "value of type string", "Example: ZioUser", "Credentials")
-                )
-              ),
-              Path(
-                "PASSWORD",
-                DescriptionsWithValue(
-                  Some(appConfig.credentials.password),
-                  List("<empty>", "value of type string", "Example: ZioPass", "Credentials")
+            NestedPath(
+              keys.credentials,
+              Both(
+                Path(
+                  keys.user,
+                  DescriptionsWithValue(
+                    Some(value.credentials.user),
+                    List(EmptySource, ConstantMap, "value of type string", "Example: ZioUser", "Credentials")
+                  )
+                ),
+                Path(
+                  keys.password,
+                  DescriptionsWithValue(
+                    Some(value.credentials.password),
+                    List(EmptySource, ConstantMap, "value of type string", "Example: ZioPass", "Credentials")
+                  )
                 )
               )
             )
           ),
-          Both(
-            Path(
-              "PORT",
-              DescriptionsWithValue(
-                Some(appConfig.database.port).map(_.toString),
-                List("<empty>", "value of type int", "Example: 8088", "Database")
-              )
-            ),
-            Path(
-              "URL",
-              DescriptionsWithValue(
-                Some(appConfig.database.url),
-                List("<empty>", "value of type string", "Example: abc.com", "Database")
+          NestedPath(
+            keys.database,
+            Both(
+              Path(
+                keys.port,
+                DescriptionsWithValue(
+                  Some(value.database.port).map(_.toString),
+                  List(EmptySource, ConstantMap, "value of type int", "Example: 8088", "Database")
+                )
+              ),
+              Path(
+                keys.url,
+                DescriptionsWithValue(
+                  Some(value.database.url),
+                  List(EmptySource, ConstantMap, "value of type string", "Example: abc.com", "Database")
+                )
               )
             )
           )
@@ -114,6 +166,19 @@ object GenerateDocsTestUtils {
       )
 
   }
+
+  private val genKey = genSymbol(1, 20)
+
+  val genConfigKeys: Gen[Random, KeyParams] =
+    for {
+      kSecret      <- genKey
+      kCredentials <- genKey
+      kUser        <- genKey
+      kPassword    <- genKey.filter(_ != kUser)
+      kDatabase    <- genKey.filter(_ != kCredentials)
+      kUrl         <- genKey
+      kPort        <- genKey.filter(_ != kDatabase)
+    } yield KeyParams(kSecret, kCredentials, kUser, kPassword, kDatabase, kUrl, kPort)
 
   private val genCredentials =
     for {
@@ -136,6 +201,7 @@ object GenerateDocsTestUtils {
 
   val generateDocsParams: Gen[Random, GenerateDocsParams] =
     for {
-      config <- genAppConfig
-    } yield GenerateDocsParams(config)
+      keys  <- genConfigKeys
+      value <- genAppConfig
+    } yield GenerateDocsParams(keys, value)
 }
