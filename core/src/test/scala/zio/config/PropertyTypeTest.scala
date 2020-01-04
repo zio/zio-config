9ebadf34b99dbf6a3fb9d9abaa44c79a05b31912
Zach Albia <zach.albia@gmail.com>
2020-01-04 10:18:49 +0800

    test: add BigIntType test

--- core/src/test/scala/zio/config/PropertyTypeTest.scala
+++ core/src/test/scala/zio/config/PropertyTypeTest.scala
@@ -10,25 +10,26 @@ import zio.test.environment.TestEnvironment
 object PropertyTypeTest
     extends BaseSpec(
       suite("Property type")(
-        testM("StringType roundtrip") {
-          check(Gen.anyString)(assertValidRoundtrip(StringType))
+        testM("StringType roundtrip") { // any string is a valid string?
+          check(Gen.anyString)(assertValidRoundtrip(StringType, equalTo))
         },
         suite("BooleanType")(
           test("valid Boolean string roundtrip") {
-            validTrueBooleans.map { s =>
-              assert(
-                BooleanType.read(s).map(BooleanType.write),
-                isRight(equalTo("true"))
-              )
-            }.reduce(_ && _) && validFalseBooleans.map { s =>
-              assert(
-                BooleanType.read(s).map(BooleanType.write),
-                isRight(equalTo("false"))
-              )
-            }.reduce(_ && _)
+            validTrueBooleans
+              .map(assertValidRoundtrip(BooleanType, _ => equalTo("true")))
+              .reduce(_ && _) &&
+            validFalseBooleans
+              .map(assertValidRoundtrip(BooleanType, _ => equalTo("false")))
+              .reduce(_ && _)
           },
           testM("invalid Boolean string roundtrip") {
-            check(genInvalidBooleanString)(assertInvalidRoundtrip(BooleanType, typeInfo = "boolean"))
+            check(genInvalidBooleanString)(
+              assertInvalidRoundtrip(
+                BooleanType,
+                typeInfo = "Boolean",
+                (info, s) => PropertyReadError(s, info.toLowerCase)
+              )
+            )
           }
         ),
         propertyTypeSpec(
@@ -55,7 +56,16 @@ object PropertyTypeTest
           Gen.anyLong,
           _.toLongOption.isEmpty
         ),
-        // TODO: Add BigInt
+        suite("BigIntType")(
+          testM("valid BigInt string roundtrip") {
+            def removeLeadingZeros(s: String) =
+              s.replaceFirst("^0+(?!$)", "")
+
+            check(genValidBigIntString)(
+              assertValidRoundtrip(BigIntType, s => equalTo(removeLeadingZeros(s)))
+            )
+          }
+        ),
         propertyTypeSpec(
           typeInfo = "Float",
           FloatType,
@@ -81,27 +91,43 @@ object PropertyTypeTestUtils {
   ): Spec[TestEnvironment, TestFailure[Nothing], String, TestSuccess[Unit]] =
     suite(s"${typeInfo}Type")(
       testM(s"valid ${typeInfo} string roundtrip") {
-        check(genValid.map(_.toString))(assertValidRoundtrip(propType))
+        check(genValid.map(_.toString))(assertValidRoundtrip(propType, equalTo))
       },
       testM(s"invalid ${typeInfo} string roundtrip") {
-        check(genInvalidString(invalidStringPredicate))(assertInvalidRoundtrip(propType, typeInfo.toLowerCase))
+        check(genInvalidString(invalidStringPredicate))(
+          assertInvalidRoundtrip(
+            propType,
+            typeInfo.toLowerCase,
+            (info, s) => PropertyReadError(s, info)
+          )
+        )
       }
     )
 
-  def assertValidRoundtrip[A](propType: PropertyType[String, A])(s: String): TestResult =
-    assert(roundTrip(propType, s), isRight(equalTo(s)))
+  def assertValidRoundtrip[A](
+    propType: PropertyType[String, A],
+    stringAssertion: String => Assertion[String]
+  )(s: String): TestResult =
+    assert(roundTrip(propType, s), isRight(stringAssertion(s)))
 
-  def assertInvalidRoundtrip[A](propType: PropertyType[String, A], typeInfo: String)(s: String): TestResult =
-    assert(roundTrip(propType, s), isLeft(equalTo(PropertyReadError(s, typeInfo))))
+  def assertInvalidRoundtrip[A](
+    propType: PropertyType[String, A],
+    typeInfo: String,
+    propReadError: (String, String) => PropertyReadError[String]
+  )(s: String): TestResult =
+    assert(
+      roundTrip(propType, s),
+      isLeft(equalTo(propReadError(typeInfo, s)))
+    )
 
   private def roundTrip[A](propType: PropertyType[String, A], s: String) =
     propType.read(s).map(propType.write)
 
-  val validTrueBooleans: List[String]  = stringCasePermutations("true")
-  val validFalseBooleans: List[String] = stringCasePermutations("false")
+  val validTrueBooleans: List[String]  = casePermutations("true")
+  val validFalseBooleans: List[String] = casePermutations("false")
 
   /** Generates all case permutations of a string */
-  private def stringCasePermutations(s: String): List[String] = {
+  private def casePermutations(s: String): List[String] = {
     val max          = 1 << s.length
     val s_           = s.toLowerCase
     val combinations = Array.ofDim[String](max)
@@ -123,6 +149,9 @@ object PropertyTypeTestUtils {
 
   val genInvalidByteString: Gen[Random with Sized, String] =
     genInvalidString(_.toByteOption.isEmpty)
+
+  val genValidBigIntString: Gen[Random with Sized, String] =
+    Gen.listOf1(Gen.char('0', '9')).map(_.mkString)
 
   def genInvalidString(predicate: String => Boolean): Gen[Random with Sized, String] =
     Gen.anyString.filter(predicate)
