929adef49e71fdf08d4cfea469659e75497d2644
afsalthaj <afsal.taj06@gmail.com>
2020-01-30 00:55:16 +1100

    Merging values, retrying different names, and types etc for typesafe config

--- examples/src/main/scala/zio/config/examples/typesafe/NullAndOptionalConfig.scala
+++ examples/src/main/scala/zio/config/examples/typesafe/NullAndOptionalConfig.scala
@@ -9,15 +9,20 @@ import EmployeeDetails._
 import zio.config.ConfigDescriptor._
 
 final case class EmployeeDetails(employees: List[Employee], accountId: Int)
-final case class Employee(name: String, state: Option[Either[Int, String]], confidence: Either[Either[Double, Int], String])
+final case class Employee(
+  name: String,
+  state: Option[Either[Int, String]],
+  confidence: Either[Either[Double, Int], String]
+)
 
 object EmployeeDetails {
   val employee: ConfigDescriptor[String, String, Employee] =
     (string("name") |@|
       int("state").orElseEither(string("state")).optional |@|
-      double("confidence").orElseEither(int("confidence")) // The value can be Double or Integer for key confidence
+      double("confidence")
+        .orElseEither(int("confidence")) // The value can be Double or Integer for key confidence
         .orElseEither(
-          string("confidence")                             // Or it could be string, but this time the key can be confidence, confidences or confs!
+          string("confidence") // Or it could be string, but this time the key can be confidence, confidences or confs!
             .orElse(string("confidences"))
             .orElse(string("confs"))
         ))(
@@ -71,7 +76,7 @@ object NullAndOptionalConfig extends App {
       Right(
         EmployeeDetails(
           List(
-            Employee("jon", Some(Right("CA")), Left(1.278)),
+            Employee("jon", Some(Right("CA")), Left(Left(1.278))),
             Employee("chris", Some(Left(151)), Right("High")),
             Employee("martha", None, Right("Medium")),
             Employee("susan", None, Right("Low"))
