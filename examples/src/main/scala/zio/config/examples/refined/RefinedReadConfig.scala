3c569d5a007ff02e6192f37214fa7c6fde6a649b
ioleo <ioleo+git@protonmail.com>
2020-03-05 13:05:42 +0100

    Refactor example

--- examples/src/main/scala/zio/config/examples/refined/RefinedReadConfig.scala
+++ examples/src/main/scala/zio/config/examples/refined/RefinedReadConfig.scala
@@ -32,25 +32,24 @@ object RefinedReadConfig extends App {
   val myAppLogic: ZIO[RefinedProd, Nothing, Refined[List[Long], Size[Greater[W.`2`.T]]]] =
     ZIO.access[RefinedProd](_.longs)
 
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ZIO.accessM { _ =>
-      val configMultiMap =
-        Map(
-          "LDAP"     -> ::("ldap", Nil),
-          "PORT"     -> ::("1999", Nil),
-          "DB_URL"   -> ::("ddd", Nil),
-          "LONGVALS" -> ::("1234", List("2345", "3456"))
-        )
-
-      val outcome: ZIO[Any, ReadErrors[Vector[String], String], Refined[List[Long], Size[Greater[W.`2`.T]]]] =
-        for {
-          config <- read(prodConfig.from(ConfigSource.fromMultiMap(configMultiMap)))
-          r      <- myAppLogic.provide(config)
-        } yield r
-
-      outcome.foldM(
-        failure => console.putStrLn(failure.toString) *> ZIO.succeed(1),
-        r => console.putStrLn(s"👍 $r") *> ZIO.succeed(0)
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
+    val configMultiMap =
+      Map(
+        "LDAP"     -> ::("ldap", Nil),
+        "PORT"     -> ::("1999", Nil),
+        "DB_URL"   -> ::("ddd", Nil),
+        "LONGVALS" -> ::("1234", List("2345", "3456"))
       )
-    }
+
+    val outcome: ZIO[Any, ReadErrors[Vector[String], String], Refined[List[Long], Size[Greater[W.`2`.T]]]] =
+      for {
+        config <- read(prodConfig.from(ConfigSource.fromMultiMap(configMultiMap)))
+        r      <- myAppLogic.provide(config)
+      } yield r
+
+    outcome.foldM(
+      failure => console.putStrLn(failure.toString) *> ZIO.succeed(1),
+      r => console.putStrLn(s"👍 $r") *> ZIO.succeed(0)
+    )
+  }
 }
