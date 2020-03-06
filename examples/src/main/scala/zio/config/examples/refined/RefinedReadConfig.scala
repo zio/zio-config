3912b778f058048724b698ca4dfe84272ff1420e
Afsal Thaj <afsal.taj06@gmail.com>
2020-03-06 11:32:20 +1100

    Update ZIO to 1.0.0-RC18 (#203)
    * Update ZIO to 1.0.0-RC18
    
    * Fix docs & 2.12 tests
    
    * Refactor example
    
    Co-authored-by: Afsal Thaj <afsal.taj06@gmail.com>

--- examples/src/main/scala/zio/config/examples/refined/RefinedReadConfig.scala
+++ examples/src/main/scala/zio/config/examples/refined/RefinedReadConfig.scala
@@ -7,9 +7,10 @@ import eu.timepit.refined.numeric.{ Greater, GreaterEqual }
 import zio.config.ConfigDescriptor.{ int, list, long, string }
 import zio.config.refined.{ greaterEqual, nonEmpty, size }
 import zio.config.{ read, ConfigDescriptor, ConfigSource, ReadErrors }
-import zio.{ App, ZEnv, ZIO }
+import zio.{ console, App, ZEnv, ZIO }
 
 object RefinedReadConfig extends App {
+
   case class RefinedProd(
     ldap: Refined[String, NonEmpty],
     port: Refined[Int, GreaterEqual[W.`1024`.T]],
@@ -31,24 +32,24 @@ object RefinedReadConfig extends App {
   val myAppLogic: ZIO[RefinedProd, Nothing, Refined[List[Long], Size[Greater[W.`2`.T]]]] =
     ZIO.access[RefinedProd](_.longs)
 
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ZIO.accessM { env =>
-      val configMultiMap =
-        Map(
-          "LDAP"     -> ::("ldap", Nil),
-          "PORT"     -> ::("1999", Nil),
-          "DB_URL"   -> ::("ddd", Nil),
-          "LONGVALS" -> ::("1234", List("2345", "3456"))
-        )
-      val outcome: ZIO[Any, ReadErrors[Vector[String], String], Refined[List[Long], Size[Greater[W.`2`.T]]]] =
-        for {
-          config <- read(prodConfig.from(ConfigSource.fromMultiMap(configMultiMap)))
-          r      <- myAppLogic.provide(config)
-        } yield r
-
-      outcome.foldM(
-        failure => env.console.putStrLn(failure.toString) *> ZIO.succeed(1),
-        r => env.console.putStrLn(s"👍 $r") *> ZIO.succeed(0)
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
+    val configMultiMap =
+      Map(
+        "LDAP"     -> ::("ldap", Nil),
+        "PORT"     -> ::("1999", Nil),
+        "DB_URL"   -> ::("ddd", Nil),
+        "LONGVALS" -> ::("1234", List("2345", "3456"))
       )
-    }
+
+    val outcome: ZIO[Any, ReadErrors[Vector[String], String], Refined[List[Long], Size[Greater[W.`2`.T]]]] =
+      for {
+        config <- read(prodConfig.from(ConfigSource.fromMultiMap(configMultiMap)))
+        r      <- myAppLogic.provide(config)
+      } yield r
+
+    outcome.foldM(
+      failure => console.putStrLn(failure.toString) *> ZIO.succeed(1),
+      r => console.putStrLn(s"👍 $r") *> ZIO.succeed(0)
+    )
+  }
 }
