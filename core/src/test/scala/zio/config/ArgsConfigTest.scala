fdf925d680268a9c466fed9eefad17926a1b6154
leigh-perry <lperry.breakpoint@gmail.com>
2020-04-03 14:14:19 +1100

    Add ArgsConfigTest

--- core/src/test/scala/zio/config/ArgsConfigTest.scala
+++ core/src/test/scala/zio/config/ArgsConfigTest.scala
@@ -1,10 +1,9 @@
 package zio.config
 
-import zio.config.ConfigDescriptor.{boolean, nested, string}
 import zio.config.ReadError.ConversionError
-import zio.random.Random
+import zio.config.testsupport.MapConfigTestSupport.AppConfig.descriptor
+import zio.config.testsupport.MapConfigTestSupport.{AppConfig, genAppConfig}
 import zio.test.Assertion._
-import zio.test.Gen.alphaNumericChar
 import zio.test.environment.TestEnvironment
 import zio.test.{DefaultRunnableSpec, _}
 import zio.{IO, ZIO}
@@ -17,7 +16,7 @@ object ArgsConfigTest extends DefaultRunnableSpec {
           val p2: zio.IO[ReadError[String], AppConfig] =
             for {
               args   <- toArgs(AppConfig.descriptor, appConfig)
-              reread <- AppConfig.fromArgs(args)
+              reread <- fromArgs(args)
             } yield reread.get
 
           assertM(p2.either)(isRight(equalTo(appConfig)))
@@ -25,107 +24,8 @@ object ArgsConfigTest extends DefaultRunnableSpec {
       }
     )
 
-  def genAppConfig: Gen[Random with Sized, AppConfig] =
-    for {
-      strings   <- Gen.listOfN(10)(stringN(1, 15))
-      supervise <- Gen.boolean
-    } yield AppConfig(
-      awsKey = strings(0),
-      awsSecret = strings(1),
-      inputTopic = strings(2),
-      jobConfig = JobConfig(
-        dataflowConfig = Some(
-          DataflowConfig(
-            name = strings(3),
-            project = strings(4),
-            region = strings(5),
-            zone = strings(6),
-            subnet = strings(7),
-            gcpTempLocation = strings(8)
-          )
-        ),
-        supervise = supervise
-      ),
-      outputTopic = strings(9)
-    )
-
-  final case class AppConfig(
-    awsKey: String,
-    awsSecret: String,
-    inputTopic: String,
-    jobConfig: JobConfig,
-    outputTopic: String
-  )
-
-  object AppConfig {
-    def fromArgs(args: List[String]): ZIO[Any, ReadError[String], Config[AppConfig]] =
-      ZIO.environment.provideLayer(Config.fromArgs(args, descriptor))
-
-    private val prefix: String        = "SystemF"
-    private val prefixAws: String     = concat(prefix, "aws")
-    private val prefixKinesis: String = concat(prefixAws, "kinesis")
-    private val prefixPubsub: String  = concat(prefix, "ps")
-
-    val descriptor: ConfigDescriptor[String, String, AppConfig] =
-      (
-        string(concat(prefixAws, "key")) |@|
-          string(concat(prefixAws, "secret")) |@|
-          string(concat(prefixKinesis, "input_topic")) |@|
-          nested(prefix) { JobConfig.descriptor } |@|
-          string(concat(prefixPubsub, "output_topic"))
-      )(AppConfig.apply, AppConfig.unapply)
-  }
-
-  final case class DataflowConfig(
-    name: String,
-    project: String,
-    region: String,
-    zone: String,
-    subnet: String,
-    gcpTempLocation: String
-  )
-
-  object DataflowConfig {
-    private val prefix = "df"
-
-    val descriptor: ConfigDescriptor[String, String, DataflowConfig] =
-      (
-        string(concat(prefix, "name")) |@|
-          string(concat(prefix, "project")) |@|
-          string(concat(prefix, "region")) |@|
-          string(concat(prefix, "zone")) |@|
-          string(concat(prefix, "subnet")) |@|
-          string(concat(prefix, "gcp_temp_location"))
-      )(DataflowConfig.apply, DataflowConfig.unapply)
-  }
-
-  ////
-
-  final case class JobConfig(
-    dataflowConfig: Option[DataflowConfig],
-    supervise: Boolean
-  )
-
-  object JobConfig {
-    private val prefix = "job"
-
-    val descriptor: ConfigDescriptor[String, String, JobConfig] =
-      (
-        nested(prefix)(DataflowConfig.descriptor).optional |@|
-          boolean(concat(prefix, "supervise"))
-      )(JobConfig.apply, JobConfig.unapply)
-  }
-
-  val separator: String = "_"
-
-  def concat(ss: String*): String =
-    ss.mkString(separator)
-
-  def stringN(min: Int, max: Int): Gen[Random with Sized, String] =
-    for {
-      n <- Gen.int(min, max)
-      s <- Gen.stringN(n)(alphaNumericChar)
-    } yield s
+  def fromArgs(args: List[String]): ZIO[Any, ReadError[String], Config[AppConfig]] =
+    ZIO.environment.provideLayer(Config.fromArgs(args, descriptor, Some('_'), None))
 
   def toArgs[A](
     descriptor: ConfigDescriptor[String, String, A],
@@ -139,7 +39,7 @@ object ArgsConfigTest extends DefaultRunnableSpec {
 
   def propertyTreeArgs(propertyTree: PropertyTree[String, String]): List[String] =
     propertyTree.flatten.toList.map { t: (Vector[String], ::[String]) =>
-      s"--${t._1.mkString(separator)}=${t._2.mkString(separator)}"
+      s"--${t._1.mkString("_")}=${t._2.mkString}"
     }
 
 }
--- core/src/test/scala/zio/config/MapConfigTest.scala
+++ core/src/test/scala/zio/config/MapConfigTest.scala
@@ -1,23 +1,23 @@
 package zio.config
 
-import zio.config.ConfigDescriptor.{ boolean, nested, string }
 import zio.config.ReadError.ConversionError
-import zio.random.Random
+import zio.config.testsupport.MapConfigTestSupport.AppConfig.descriptor
 import zio.test.Assertion._
-import zio.test.Gen.alphaNumericChar
+import zio.test._
 import zio.test.environment.TestEnvironment
-import zio.test.{ DefaultRunnableSpec, _ }
 import zio.{ IO, ZIO }
 
 object MapConfigTest extends DefaultRunnableSpec {
+  import zio.config.testsupport.MapConfigTestSupport._
+
   def spec: Spec[TestEnvironment, TestFailure[Nothing], TestSuccess] =
-    suite("Configuration from command-line-style arguments")(
-      testM("Configuration from arguments roundtrip") {
+    suite("Configuration from Map")(
+      testM("Configuration from Map roundtrip") {
         checkM(genAppConfig) { appConfig =>
           val p2: zio.IO[ReadError[String], AppConfig] =
             for {
               args   <- toMap(AppConfig.descriptor, appConfig)
-              reread <- AppConfig.fromMap(args)
+              reread <- fromMap(args)
             } yield reread.get
 
           assertM(p2.either)(isRight(equalTo(appConfig)))
@@ -25,107 +25,8 @@ object MapConfigTest extends DefaultRunnableSpec {
       }
     )
 
-  def genAppConfig: Gen[Random with Sized, AppConfig] =
-    for {
-      strings   <- Gen.listOfN(10)(stringN(1, 15))
-      supervise <- Gen.boolean
-    } yield AppConfig(
-      awsKey = strings(0),
-      awsSecret = strings(1),
-      inputTopic = strings(2),
-      jobConfig = JobConfig(
-        dataflowConfig = Some(
-          DataflowConfig(
-            name = strings(3),
-            project = strings(4),
-            region = strings(5),
-            zone = strings(6),
-            subnet = strings(7),
-            gcpTempLocation = strings(8)
-          )
-        ),
-        supervise = supervise
-      ),
-      outputTopic = strings(9)
-    )
-
-  final case class AppConfig(
-    awsKey: String,
-    awsSecret: String,
-    inputTopic: String,
-    jobConfig: JobConfig,
-    outputTopic: String
-  )
-
-  object AppConfig {
-    def fromMap(args: Map[String, String]): ZIO[Any, ReadError[String], Config[AppConfig]] =
-      ZIO.environment.provideLayer(Config.fromMap(args, descriptor, "WTL", None, None))
-
-    private val prefix: String        = "SystemF"
-    private val prefixAws: String     = concat(prefix, "aws")
-    private val prefixKinesis: String = concat(prefixAws, "kinesis")
-    private val prefixPubsub: String  = concat(prefix, "ps")
-
-    val descriptor: ConfigDescriptor[String, String, AppConfig] =
-      (
-        string(concat(prefixAws, "key")) |@|
-          string(concat(prefixAws, "secret")) |@|
-          string(concat(prefixKinesis, "input_topic")) |@|
-          nested(prefix) { JobConfig.descriptor } |@|
-          string(concat(prefixPubsub, "output_topic"))
-      )(AppConfig.apply, AppConfig.unapply)
-  }
-
-  final case class DataflowConfig(
-    name: String,
-    project: String,
-    region: String,
-    zone: String,
-    subnet: String,
-    gcpTempLocation: String
-  )
-
-  object DataflowConfig {
-    private val prefix = "df"
-
-    val descriptor: ConfigDescriptor[String, String, DataflowConfig] =
-      (
-        string(concat(prefix, "name")) |@|
-          string(concat(prefix, "project")) |@|
-          string(concat(prefix, "region")) |@|
-          string(concat(prefix, "zone")) |@|
-          string(concat(prefix, "subnet")) |@|
-          string(concat(prefix, "gcp_temp_location"))
-      )(DataflowConfig.apply, DataflowConfig.unapply)
-  }
-
-  ////
-
-  final case class JobConfig(
-    dataflowConfig: Option[DataflowConfig],
-    supervise: Boolean
-  )
-
-  object JobConfig {
-    private val prefix = "job"
-
-    val descriptor: ConfigDescriptor[String, String, JobConfig] =
-      (
-        nested(prefix)(DataflowConfig.descriptor).optional |@|
-          boolean(concat(prefix, "supervise"))
-      )(JobConfig.apply, JobConfig.unapply)
-  }
-
-  val separator: String = "_"
-
-  def concat(ss: String*): String =
-    ss.mkString(separator)
-
-  def stringN(min: Int, max: Int): Gen[Random with Sized, String] =
-    for {
-      n <- Gen.int(min, max)
-      s <- Gen.stringN(n)(alphaNumericChar)
-    } yield s
+  def fromMap(args: Map[String, String]): ZIO[Any, ReadError[String], Config[AppConfig]] =
+    ZIO.environment.provideLayer(Config.fromMap(args, descriptor, "WTL", Some('_'), None))
 
   def toMap[A](
     descriptor: ConfigDescriptor[String, String, A],
@@ -136,11 +37,11 @@ object MapConfigTest extends DefaultRunnableSpec {
         s => ConversionError[String](Vector(Left(0)), s),
         propertyTreeArgs
       )
-      .map(tuples => Map(tuples : _*))
+      .map(tuples => Map(tuples: _*))
 
   def propertyTreeArgs(propertyTree: PropertyTree[String, String]): List[(String, String)] =
     propertyTree.flatten.toList.map { t: (Vector[String], ::[String]) =>
-      (s"${t._1.mkString(separator)}", s"${t._2.mkString(separator)}")
+      (s"${t._1.mkString("_")}", s"${t._2.mkString}")
     }
 
 }
