82fe47693a088bbb55c30299a2e66ecd7cba0f83
afsalthaj <afsal.taj06@gmail.com>
2020-04-05 20:07:55 +1000

    Make a complex example

--- examples/src/main/scala/zio/config/examples/commandline/CommandLineArgsExample.scala
+++ examples/src/main/scala/zio/config/examples/commandline/CommandLineArgsExample.scala
@@ -1,13 +1,13 @@
 package zio.config.examples.commandline
 
-import zio.config.ConfigDescriptor.{ list, nested, string }
+import zio.config.ConfigDescriptor._
 import zio.config.{ read, ConfigDescriptor, ConfigSource }
 
 object CommandLineArgs extends App {
   val argss =
-    "--k --l --z --x -database.username=1 --k --l --z --x.database.password=hi --k --l --z --x        -database.url=jdbc://xyz --vault -username=3 --vault -password=10 --vault -something=11 --users 100 --region 111,122"
+    "--conf -database.username=1 --conf -database.password=hi --conf.database.url=jdbc://xyz --conf -num_execs=10 --vault.username=3 --vault.password=10 --vault.something=11 --users 100 --region 111,122"
 
-  val source = ConfigSource.fromCommandLineArgs(argss.split(' '), Some('.'), Some(','))
+  val source = ConfigSource.fromCommandLineArgs(argss.split(' ').toList, Some('.'), Some(','))
 
   final case class UserPassword(k2: String, k3: String)
 
@@ -32,15 +32,17 @@ object CommandLineArgs extends App {
       }(VaultConfig.apply, VaultConfig.unapply)
   }
 
-  final case class AppConfig(databaseConfig: DatabaseConfig, vault: VaultConfig, users: String, region: List[String])
+  final case class SparkConfig(databaseConfig: DatabaseConfig, numberOfExecutors: Int)
+
+  object SparkConfig {
+    val desc = (DatabaseConfig.desc |@| int("num_execs"))(SparkConfig.apply, SparkConfig.unapply)
+  }
+
+  final case class AppConfig(sparkConfig: SparkConfig, vault: VaultConfig, users: String, region: List[String])
 
   object AppConfig {
     val desc: ConfigDescriptor[String, String, AppConfig] =
-      (nested("k") { nested("l") { nested("z") { nested("x") { DatabaseConfig.desc } } } } |@| VaultConfig.desc |@| string(
-        "users"
-      ) |@| list(
-        string("region")
-      ))(
+      (nested("conf") { SparkConfig.desc } |@| VaultConfig.desc |@| string("users") |@| list(string("region")))(
         AppConfig.apply,
         AppConfig.unapply
       )
@@ -50,7 +52,7 @@ object CommandLineArgs extends App {
     read(AppConfig.desc from (source)) ==
       Right(
         AppConfig(
-          DatabaseConfig(UserPassword("1", "hi"), "jdbc://xyz"),
+          SparkConfig(DatabaseConfig(UserPassword("1", "hi"), "jdbc://xyz"), 10),
           VaultConfig(UserPassword("3", "10")),
           "100",
           List("111", "122")
