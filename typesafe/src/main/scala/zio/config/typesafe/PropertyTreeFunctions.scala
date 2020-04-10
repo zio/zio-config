8dab7e913be35fbe7a019754d8c6c09baf5073e6
afsalthaj <afsal.taj06@gmail.com>
2020-04-11 01:16:16 +1000

    Add map support

--- typesafe/src/main/scala/zio/config/typesafe/PropertyTreeFunctions.scala
+++ typesafe/src/main/scala/zio/config/typesafe/PropertyTreeFunctions.scala
@@ -8,56 +8,56 @@ import scala.collection.JavaConverters._
 import scala.collection.immutable.Nil
 
 private[typesafe] trait PropertyTreeFunctions {
-  def loopLeaf(keys: List[String], tree: Leaf[String]): ConfigObject =
-    keys.headOption
+  def treeToTypesafeConfig(
+                            tree: PropertyTree[String, String]
+                          ): com.typesafe.config.ConfigObject =
+    loopAny(tree, None)
+
+  def loopAny(tree: PropertyTree[String, String], key: Option[String]): com.typesafe.config.ConfigObject =
+    tree match {
+      case leaf @ Leaf(_)     => loopLeaf(key, leaf)
+      case record @ Record(_) => loopRecord(record)
+      case PropertyTree.Empty => ConfigFactory.empty().root()
+      case seqq @ Sequence(_) => loopSequence(key, seqq)
+    }
+
+  def loopLeaf(key: Option[String], tree: Leaf[String]): ConfigObject =
+    key
       .fold(ConfigFactory.empty())(
         last => ConfigFactory.empty().withValue(last, ConfigValueFactory.fromAnyRef(tree.value))
       )
       .root()
 
-  def loopRecord(keys: List[String], tree: Record[String, String]): ConfigObject =
+  def loopRecord(tree: Record[String, String]): ConfigObject =
     tree.value.toList.foldLeft(ConfigFactory.empty().root()) {
       case (acc, (k, tree)) =>
         val newObject =
-          loopAny(tree, List(k))
+          loopAny(tree, Some(k))
 
         acc.withValue(k, newObject.getOrDefault(k, newObject))
     }
 
-  def loopSequence(keys: List[String], tree: Sequence[String, String]): ConfigObject =
-    keys.headOption match {
-      case Some(head) =>
+  def loopSequence(key: Option[String], tree: Sequence[String, String]): ConfigObject =
+    key match {
+      case Some(key) =>
         val leaves = partitionWith(tree.value) {
           case Leaf(value) => Leaf(value)
         }
 
         if (leaves.nonEmpty)
-          ConfigFactory.empty().withValue(head, ConfigValueFactory.fromIterable(leaves.map(_.value).asJava)).root()
+          ConfigFactory.empty().withValue(key, ConfigValueFactory.fromIterable(leaves.map(_.value).asJava)).root()
         else {
           ConfigFactory
             .empty()
             .withValue(
-              head,
-              ConfigValueFactory.fromIterable(tree.value.map(loopAny(_, keys.tail)).asJava)
+              key,
+              ConfigValueFactory.fromIterable(tree.value.map(loopAny(_, None)).asJava)
             )
             .root()
         }
 
       case None => ConfigFactory.empty().root()
     }
-
-  def loopAny(tree: PropertyTree[String, String], keys: List[String]): com.typesafe.config.ConfigObject =
-    tree match {
-      case leaf @ Leaf(_)     => loopLeaf(keys, leaf)
-      case record @ Record(_) => loopRecord(keys, record)
-      case PropertyTree.Empty => ConfigFactory.empty().root()
-      case seqq @ Sequence(_) => loopSequence(keys, seqq)
-    }
-
-  def treeToTypesafeConfig(
-    tree: PropertyTree[String, String]
-  ): com.typesafe.config.ConfigObject =
-    loopAny(tree, List.empty)
 
   def partitionWith[K, V, A](
     trees: List[PropertyTree[K, V]]
