ec3863fb0f9c8b882fcee17f20a06d9a51386296
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2020-05-17 09:37:47 +0900

    Replace deprecated Tagged by Tag (#334)
    * Upgrade ZIO to RC19
    
    * Replace deprecated Tagged by Tag

--- typesafe/src/main/scala/zio/config/typesafe/TypesafeConfig.scala
+++ typesafe/src/main/scala/zio/config/typesafe/TypesafeConfig.scala
@@ -4,31 +4,31 @@ import java.io.File
 
 import com.typesafe.config.ConfigFactory
 import zio.config.Config
-import zio.{ Layer, Tagged, ZIO }
+import zio.{ Layer, Tag, ZIO }
 import zio.config.ConfigDescriptor
 
 object TypesafeConfig {
   def fromDefaultLoader[A](
     configDescriptor: ConfigDescriptor[A]
-  )(implicit tagged: Tagged[A]): Layer[Throwable, Config[A]] =
+  )(implicit tag: Tag[A]): Layer[Throwable, Config[A]] =
     fromTypesafeConfig(ConfigFactory.load.resolve, configDescriptor)
 
   def fromHoconFile[A](
     file: File,
     configDescriptor: ConfigDescriptor[A]
-  )(implicit tagged: Tagged[A]): Layer[Throwable, Config[A]] =
+  )(implicit tag: Tag[A]): Layer[Throwable, Config[A]] =
     fromTypesafeConfig(ConfigFactory.parseFile(file).resolve, configDescriptor)
 
   def fromHoconString[A](
     str: String,
     configDescriptor: ConfigDescriptor[A]
-  )(implicit tagged: Tagged[A]): Layer[Throwable, Config[A]] =
+  )(implicit tag: Tag[A]): Layer[Throwable, Config[A]] =
     fromTypesafeConfig(ConfigFactory.parseString(str).resolve, configDescriptor)
 
   def fromTypesafeConfig[A](
     conf: => com.typesafe.config.Config,
     configDescriptor: ConfigDescriptor[A]
-  )(implicit tagged: Tagged[A]): Layer[Throwable, Config[A]] =
+  )(implicit tag: Tag[A]): Layer[Throwable, Config[A]] =
     Config.fromConfigDescriptorM(
       ZIO
         .fromEither(TypesafeConfigSource.fromTypesafeConfig(conf))
