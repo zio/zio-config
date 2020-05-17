46a84c5269cd8374cf5f24874d9943bad627468d
Pierre Ricadat <pierre.ricadat@devsisters.com>
2020-05-17 09:27:51 +0900

    Replace deprecated Tagged by Tag

--- core/src/main/scala/zio/config/syntax/package.scala
+++ core/src/main/scala/zio/config/syntax/package.scala
@@ -1,11 +1,11 @@
 package zio.config
 
-import zio.{ Has, Tagged, ZLayer }
+import zio.{ Has, Tag, ZLayer }
 
 package object syntax {
 
   final implicit class ZIOConfigNarrowOps[R, E, A](val self: ZLayer[R, E, Has[A]]) extends AnyVal {
-    def narrow[B: Tagged](f: A => B)(implicit T: Tagged[A]): ZLayer[R, E, Has[B]] =
+    def narrow[B: Tag](f: A => B)(implicit T: Tag[A]): ZLayer[R, E, Has[B]] =
       self.map(a => Has(f(a.get)))
   }
 }
