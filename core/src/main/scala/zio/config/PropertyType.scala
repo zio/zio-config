042d47c9fbfe5f1356424cd1dbbf28d7dbc7c965
d948521 <afsal.taj06@gmail.com>
2020-09-13 15:40:33 +1000

    Update zio to 1.0.1

--- core/src/main/scala/zio/config/PropertyType.scala
+++ core/src/main/scala/zio/config/PropertyType.scala
@@ -102,7 +102,7 @@ object PropertyType {
   case object ZioDurationType extends PropertyType[String, zio.duration.Duration] {
     def read(value: String): Either[PropertyReadError[String], zio.duration.Duration] =
       attempt(zio.duration.Duration.fromScala(Duration.apply(value)), _ => PropertyReadError(value, "duration"))
-    def write(value: zio.duration.Duration): String = value.render
+    def write(value: zio.duration.Duration): String = value.toString
   }
 
   case object LocalDateType extends PropertyType[String, LocalDate] {
