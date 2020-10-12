129e4ce48fa3818cca82f3873905835db66dbdd0
ShankarShastri <shastri.shankar9@gmail.com>
2020-10-12 09:56:54 +0530

    Making last explicit as a string in loopLeaf

--- typesafe/src/main/scala/zio/config/typesafe/TreeToHoconSupport.scala
+++ typesafe/src/main/scala/zio/config/typesafe/TreeToHoconSupport.scala
@@ -29,7 +29,7 @@ private[typesafe] trait TreeToHoconSupport {
   def loopLeaf(key: Option[String], tree: Leaf[String]): ConfigObject =
     key
       .fold(ConfigFactory.empty())(
-        last => ConfigFactory.empty().withValue(last, ConfigValueFactory.fromAnyRef(tree.value))
+        last => ConfigFactory.empty().withValue(s""" "${last}" """, ConfigValueFactory.fromAnyRef(tree.value))
       )
       .root()
 
