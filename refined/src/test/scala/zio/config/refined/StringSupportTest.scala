db84414e6e6da0c723f0b2499817d252d6525f5e
Afsal Thaj <afsal.taj06@gmail.com>
2020-11-22 14:07:45 +1100

    Make refined module user friendly (#445)
    * Make refined module user friendly
    
    * Fix API docs for refined
    
    * Fix examples

--- refined/src/test/scala/zio/config/refined/StringSupportTest.scala
+++ refined/src/test/scala/zio/config/refined/StringSupportTest.scala
@@ -4,59 +4,20 @@ import java.util.UUID
 
 import eu.timepit.refined.W
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.string.{
-  EndsWith,
-  IPv4,
-  MatchesRegex,
-  Regex,
-  Uri,
-  Url,
-  Uuid,
-  ValidBigDecimal,
-  ValidBigInt,
-  ValidByte,
-  ValidDouble,
-  ValidFloat,
-  ValidInt,
-  ValidLong,
-  ValidShort
-}
+import eu.timepit.refined.string._
 import zio.ZIO
 import zio.config.helpers._
-import zio.config.{ helpers, BaseSpec, ConfigSource }
+import zio.config.{ helpers, BaseSpec }
 import zio.test.Assertion._
-import zio.config.{ read, write, ConfigDescriptor, LeafForSequence }, ConfigDescriptor._
+import zio.config._
 import zio.test._
 
 object StringSupportTest
     extends BaseSpec(
       suite("Refined String support")(
-        testM("Refined config EndsWith roundtrip") {
-          checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, EndsWith[W.`"abc"`.T]](s + "abc"))) { p =>
-            val cfg = endsWith[W.`"abc"`.T](string("TEST"))
-            val p2 =
-              for {
-                written <- ZIO.fromEither(write(cfg, p))
-                reread <- ZIO.fromEither(
-                           read(cfg from ConfigSource.fromPropertyTree(written, "tree", LeafForSequence.Valid))
-                         )
-              } yield reread
-
-            assertM(p2)(equalTo(p))
-          }
-        },
-        testM("Refined config EndsWith invalid") {
-          check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = endsWith[W.`"abc"`.T](string("TEST"))
-            val p2 =
-              read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
-
-            assert(p2)(helpers.assertErrors(_.size == 1))
-          }
-        },
         testM("Refined config IPv4 roundtrip") {
           checkM(Gen.listOfN(4)(Gen.int(0, 255)).map(s => Refined.unsafeApply[String, IPv4](s.mkString(".")))) { p =>
-            val cfg = iPv4(string("TEST"))
+            val cfg = refine[String, IPv4]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -70,7 +31,7 @@ object StringSupportTest
         },
         testM("Refined config IPv4 invalid") {
           check(Gen.listOfN(4)(Gen.int(256, 1000)).map(_.mkString("."))) { p =>
-            val cfg = iPv4(string("TEST"))
+            val cfg = refine[String, IPv4]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -79,7 +40,7 @@ object StringSupportTest
         },
         testM("Refined config IPv6 invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = iPv6(string("TEST"))
+            val cfg = refine[String, IPv6]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -88,7 +49,8 @@ object StringSupportTest
         },
         testM("Refined config MatchesRegex roundtrip") {
           checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, MatchesRegex[W.`".*abc"`.T]](s + "abc"))) { p =>
-            val cfg = matchesRegex[W.`".*abc"`.T](string("TEST"))
+            val cfg = refine[String, MatchesRegex[W.`".*abc"`.T]]("TEST")
+
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -102,7 +64,8 @@ object StringSupportTest
         },
         testM("Refined config MatchesRegex invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = matchesRegex[W.`".*abc"`.T](string("TEST"))
+            val cfg = refine[String, MatchesRegex[W.`".*abc"`.T]]("TEST")
+
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -111,7 +74,7 @@ object StringSupportTest
         },
         testM("Refined config Regex roundtrip") {
           checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, Regex](s + ".+abc"))) { p =>
-            val cfg = regex(string("TEST"))
+            val cfg = refine[String, Regex]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -125,7 +88,7 @@ object StringSupportTest
         },
         testM("Refined config Regex invalid") {
           check(genSymbol(0, 10).map(s => s + "\\q5ab")) { p =>
-            val cfg = regex(string("TEST"))
+            val cfg = refine[String, Regex]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -134,7 +97,7 @@ object StringSupportTest
         },
         testM("Refined config Uri roundtrip") {
           checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, Uri]("https://" + s + "w.asdf.asdf"))) { p =>
-            val cfg = uri(string("TEST"))
+            val cfg = refine[String, Uri]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -148,7 +111,7 @@ object StringSupportTest
         },
         testM("Refined config Url roundtrip") {
           checkM(genSymbol(0, 10).map(s => Refined.unsafeApply[String, Url]("https://" + s + "w.asdf.asdf"))) { p =>
-            val cfg = url(string("TEST"))
+            val cfg = refine[String, Url]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -162,7 +125,7 @@ object StringSupportTest
         },
         testM("Refined config Uuid roundtrip") {
           checkM(Gen.fromEffect(ZIO.succeed(Refined.unsafeApply[String, Uuid](UUID.randomUUID().toString)))) { p =>
-            val cfg = uuid(string("TEST"))
+            val cfg = refine[String, Uuid]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -176,7 +139,7 @@ object StringSupportTest
         },
         testM("Refined config Uuid invalid") {
           check(Gen.fromEffect(ZIO.succeed(UUID.randomUUID().toString + "ab"))) { p =>
-            val cfg = uuid(string("TEST"))
+            val cfg = refine[String, Uuid]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -185,7 +148,7 @@ object StringSupportTest
         },
         testM("Refined config ValidByte roundtrip") {
           checkM(Gen.byte(-128, 127).map(s => Refined.unsafeApply[String, ValidByte](s.toString))) { p =>
-            val cfg = validByte(string("TEST"))
+            val cfg = refine[String, ValidByte]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -199,7 +162,7 @@ object StringSupportTest
         },
         testM("Refined config ValidByte invalid") {
           check(genSymbol(0, 10).map(s => s.toString + "ab")) { p =>
-            val cfg = validByte(string("TEST"))
+            val cfg = refine[String, ValidByte]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -208,7 +171,7 @@ object StringSupportTest
         },
         testM("Refined config ValidShort roundtrip") {
           checkM(Gen.short(-128, 127).map(s => Refined.unsafeApply[String, ValidShort](s.toString))) { p =>
-            val cfg = validShort(string("TEST"))
+            val cfg = refine[String, ValidShort]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -222,7 +185,7 @@ object StringSupportTest
         },
         testM("Refined config ValidShort invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validShort(string("TEST"))
+            val cfg = refine[String, ValidShort]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -231,7 +194,7 @@ object StringSupportTest
         },
         testM("Refined config ValidInt roundtrip") {
           checkM(Gen.int(-128, 127).map(s => Refined.unsafeApply[String, ValidInt](s.toString))) { p =>
-            val cfg = validInt(string("TEST"))
+            val cfg = refine[String, ValidInt]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -245,7 +208,7 @@ object StringSupportTest
         },
         testM("Refined config ValidInt invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validInt(string("TEST"))
+            val cfg = refine[String, ValidInt]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -254,7 +217,7 @@ object StringSupportTest
         },
         testM("Refined config ValidLong roundtrip") {
           checkM(Gen.long(-128, 127).map(s => Refined.unsafeApply[String, ValidLong](s.toString))) { p =>
-            val cfg = validLong(string("TEST"))
+            val cfg = refine[String, ValidLong]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -268,7 +231,7 @@ object StringSupportTest
         },
         testM("Refined config ValidLong invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validLong(string("TEST"))
+            val cfg = refine[String, ValidLong]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -277,7 +240,7 @@ object StringSupportTest
         },
         testM("Refined config ValidFloat roundtrip") {
           checkM(Gen.long(-128, 127).map(s => Refined.unsafeApply[String, ValidFloat](s.toString + ".123"))) { p =>
-            val cfg = validFloat(string("TEST"))
+            val cfg = refine[String, ValidFloat]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -291,7 +254,7 @@ object StringSupportTest
         },
         testM("Refined config ValidFloat invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validFloat(string("TEST"))
+            val cfg = refine[String, ValidFloat]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -300,7 +263,7 @@ object StringSupportTest
         },
         testM("Refined config ValidDouble roundtrip") {
           checkM(Gen.double(-128, 127).map(s => Refined.unsafeApply[String, ValidDouble](s.toString))) { p =>
-            val cfg = validDouble(string("TEST"))
+            val cfg = refine[String, ValidDouble]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -314,7 +277,7 @@ object StringSupportTest
         },
         testM("Refined config ValidDouble invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validDouble(string("TEST"))
+            val cfg = refine[String, ValidDouble]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -323,7 +286,7 @@ object StringSupportTest
         },
         testM("Refined config ValidBigInt roundtrip") {
           checkM(Gen.long(-128, 127).map(s => Refined.unsafeApply[String, ValidBigInt](s.toString))) { p =>
-            val cfg = validBigInt(string("TEST"))
+            val cfg = refine[String, ValidBigInt]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -337,7 +300,7 @@ object StringSupportTest
         },
         testM("Refined config ValidBigInt invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validBigInt(string("TEST"))
+            val cfg = refine[String, ValidBigInt]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
@@ -346,7 +309,7 @@ object StringSupportTest
         },
         testM("Refined config ValidBigDecimal roundtrip") {
           checkM(Gen.long(-128, 127).map(s => Refined.unsafeApply[String, ValidBigDecimal](s.toString + ".123"))) { p =>
-            val cfg = validBigDecimal(string("TEST"))
+            val cfg = refine[String, ValidBigDecimal]("TEST")
             val p2 =
               for {
                 written <- ZIO.fromEither(write(cfg, p))
@@ -360,7 +323,7 @@ object StringSupportTest
         },
         testM("Refined config ValidBigDecimal invalid") {
           check(genSymbol(0, 10).map(s => s + "ab")) { p =>
-            val cfg = validBigDecimal(string("TEST"))
+            val cfg = refine[String, ValidBigDecimal]("TEST")
             val p2 =
               read(cfg from ConfigSource.fromMap(Map("TEST" -> p)))
 
