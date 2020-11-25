e9712efe48186ef4a01e57895a2ade9e173312d0
d948521 <afsal.taj06@gmail.com>
2020-11-25 14:03:58 +1100

    Format scala211 support

--- core/src/main/scala-2.11/zio/config/VersionSpecificSupport.scala
+++ core/src/main/scala-2.11/zio/config/VersionSpecificSupport.scala
@@ -1,6 +1,6 @@
 package zio.config
 
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
 
 private[config] object VersionSpecificSupport {
   implicit class RightBiasedEither[L, R](e: Either[L, R]) {
@@ -10,10 +10,10 @@ private[config] object VersionSpecificSupport {
     }
   }
 
-  implicit class TryOps[A](t : scala.util.Try[A]) {
+  implicit class TryOps[A](t: scala.util.Try[A]) {
     def toEither = t match {
       case Failure(exception) => Left(exception)
-      case Success(value) => Right(value)
+      case Success(value)     => Right(value)
     }
   }
 }
