bc611b51647507b00a73b15745e347ac8e21e1c4
d948521 <afsal.taj06@gmail.com>
2021-01-03 17:03:17 +1100

    Reformat CombineSourcesExample

--- examples/src/main/scala/zio/config/examples/CombineSourcesExample.scala
+++ examples/src/main/scala/zio/config/examples/CombineSourcesExample.scala
@@ -2,11 +2,11 @@ package zio.config.examples
 
 import java.io.File
 
-import zio.{App, ExitCode, URIO, ZIO, system}
+import zio.{ system, App, ExitCode, URIO, ZIO }
 import zio.config._
 import zio.config.magnolia._
 import zio.config.typesafe._
-import zio.console.{Console, putStrLn}
+import zio.console.{ putStrLn, Console }
 
 /**
  * One of the ways you can summon various sources especially
@@ -18,7 +18,7 @@ object CombineSourcesExample extends App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     application.either.flatMap(r => putStrLn(s"Result: ${r}")).exitCode
 
-  final case class Config(username: String , password: String)
+  final case class Config(username: String, password: String)
 
   val getConfig: ZIO[system.System, ReadError[String], _root_.zio.config.ConfigDescriptor[Config]] =
     for {
@@ -31,9 +31,9 @@ object CombineSourcesExample extends App {
 
   val application: ZIO[Console with system.System, String, Unit] =
     for {
-      desc      <- getConfig.mapError(_.prettyPrint())
+      desc        <- getConfig.mapError(_.prettyPrint())
       configValue <- ZIO.fromEither(read(desc)).mapError(_.prettyPrint())
-      string <-  ZIO.fromEither(configValue.toJson(desc))
-      _ <- putStrLn(string)
+      string      <- ZIO.fromEither(configValue.toJson(desc))
+      _           <- putStrLn(string)
     } yield ()
 }
