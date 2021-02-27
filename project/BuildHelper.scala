0c34bb79006e288cf1d69181d766d93277c14d1c
Scala Steward <me@scala-steward.org>
2021-02-27 03:13:40 +0100

    Update scala-library, scala-reflect to 2.13.5

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -76,7 +76,7 @@ object BuildHelper {
   def stdSettings(prjName: String) = Seq(
     name := s"$prjName",
     scalacOptions := stdOptions,
-    crossScalaVersions := Seq("2.13.2", "2.12.11", "2.11.12"),
+    crossScalaVersions := Seq("2.13.2", "2.12.11", "2.13.5"),
     scalaVersion in ThisBuild := crossScalaVersions.value.head,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
