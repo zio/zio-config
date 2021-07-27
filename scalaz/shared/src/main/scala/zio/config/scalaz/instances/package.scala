5786e2be087601cc8c115cce02f2e55d8e480953
afsalthaj <afsal.taj06@gmail.com>
2021-07-27 17:36:58 +1000

    Make zio-config-scalaz consistent with zio-config-cats

--- scalaz/shared/src/main/scala/zio/config/scalaz/instances/package.scala
+++ scalaz/shared/src/main/scala/zio/config/scalaz/instances/package.scala
@@ -1,9 +1,9 @@
 package zio.config.scalaz
 
 import scalaz.InvariantFunctor
-import zio.config._
+import zio.config._, ConfigDescriptor._
 
-trait ScalazInstances {
+package object instances {
   implicit val invariantConfigDescriptor: InvariantFunctor[ConfigDescriptor] =
     new InvariantFunctor[ConfigDescriptor] {
       def xmap[A, B](ma: ConfigDescriptor[A], f: A => B, g: B => A): ConfigDescriptor[B] =
--- scalaz/shared/src/main/scala/zio/config/scalaz/package.scala
+++ scalaz/shared/src/main/scala/zio/config/scalaz/package.scala
@@ -1,8 +1,22 @@
 package zio.config
 
 import _root_.scalaz.Isomorphism.<=>
+import _root_.scalaz.{==>>, IList, ISet, Maybe, Order}
+import zio.config._, ConfigDescriptor._
 
-package object scalaz extends ScalazInstances {
-  def fromIso[A, B](D: A <=> B)(implicit M: ConfigDescriptor[A]): ConfigDescriptor[B] =
+package object scalaz {
+  def fromIso[A, B](D: A <=> B)(M: ConfigDescriptor[A]): ConfigDescriptor[B] =
     M.transform[B](D.to, D.from)
+
+  def iList[A](config: ConfigDescriptor[A]): ConfigDescriptor[IList[A]] =
+    list(config).transform(IList.fromList, _.toList)
+
+  def iSet[A: Order](config: ConfigDescriptor[A]): ConfigDescriptor[ISet[A]] =
+    list(config).transform(l => ISet.fromList(l), _.toList)
+
+  def mapz[A: Order, B](config: ConfigDescriptor[Map[A, B]]): ConfigDescriptor[A ==>> B] =
+    config.transform(m => ==>>.fromList(m.toList), _.toList.toMap)
+
+  def maybe[A](config: ConfigDescriptor[A]): ConfigDescriptor[Maybe[A]] =
+    config.optional.transform(Maybe.fromOption, _.toOption)
 }
