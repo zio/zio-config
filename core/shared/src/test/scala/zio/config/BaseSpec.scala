0dcaf72e619656357b76dfeb22a9464b735f30d8
John A. De Goes <john@degoes.net>
2022-01-13 19:08:57 +0000

    Fix

--- core/shared/src/test/scala/zio/config/BaseSpec.scala
+++ core/shared/src/test/scala/zio/config/BaseSpec.scala
@@ -4,6 +4,6 @@ import zio._
 import zio.test.{Live, TestAspect, TestAspectAtLeastR, TestConfig, ZIOSpecDefault}
 
 abstract class BaseSpec extends ZIOSpecDefault {
-  override def aspects: Chunk[TestAspectAtLeastR[Live with TestConfig]] = 
+  override def aspects: Chunk[TestAspectAtLeastR[Live with TestConfig]] =
     Chunk(TestAspect.timeout(60.seconds) @@ TestAspect.samples(10))
 }
