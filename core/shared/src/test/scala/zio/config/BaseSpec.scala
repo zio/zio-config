3f3c1c480df080ac2cf7709ce33b2fe5bb9f9808
John A. De Goes <john@degoes.net>
2022-01-13 19:04:37 +0000

    Reduce samples

--- core/shared/src/test/scala/zio/config/BaseSpec.scala
+++ core/shared/src/test/scala/zio/config/BaseSpec.scala
@@ -1,8 +1,9 @@
 package zio.config
 
 import zio._
-import zio.test.{Live, TestAspect, TestAspectAtLeastR, ZIOSpecDefault}
+import zio.test.{Live, TestAspect, TestAspectAtLeastR, TestConfig, ZIOSpecDefault}
 
 abstract class BaseSpec extends ZIOSpecDefault {
-  override def aspects: Chunk[TestAspectAtLeastR[Live]] = Chunk(TestAspect.timeout(60.seconds))
+  override def aspects: Chunk[TestAspectAtLeastR[Live with TestConfig]] = 
+    Chunk(TestAspect.timeout(60.seconds) @@ TestAspect.samples(10))
 }
