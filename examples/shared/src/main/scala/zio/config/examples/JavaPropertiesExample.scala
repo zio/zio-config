c3e9520067b3a38a6cc71bd39414237a0f118068
athaj <afsal.taj06@gmail.com>
2022-04-04 22:38:48 +1000

    Remove more environments

--- examples/shared/src/main/scala/zio/config/examples/JavaPropertiesExample.scala
+++ examples/shared/src/main/scala/zio/config/examples/JavaPropertiesExample.scala
@@ -43,14 +43,14 @@ object JavaPropertiesExample extends ZIOAppDefault {
 // The core application functions
 object SimpleExample {
 
-  val printConfigs: ZIO[ApplicationConfig with Console, IOException, Unit] =
+  val printConfigs: ZIO[ApplicationConfig, IOException, Unit] =
     for {
       appConfig <- getConfig[ApplicationConfig]
       _         <- Console.printLine(appConfig.bridgeIp)
       _         <- Console.printLine(appConfig.userName)
     } yield ()
 
-  val finalExecution: ZIO[ApplicationConfig with Console, IOException, Unit] =
+  val finalExecution: ZIO[ApplicationConfig, IOException, Unit] =
     for {
       _ <- printConfigs
       _ <- Console.printLine(s"processing data......")
