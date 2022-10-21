83677e8cb0454b3e9cdb6c7075da2cb76a7a80d2
Fredrik Wärnsberg <fredrik@warnsberg.se>
2022-10-21 09:22:58 +0200

    fix: update zip alias (#933)
    * add test that zip != <*>
    
    * update <*> to match zip
    
    * update test
    
    Co-authored-by: Afsal Thaj <afsal.taj06@gmail.com>

--- core/shared/src/test/scala/zio/config/SyntaxTest.scala
+++ core/shared/src/test/scala/zio/config/SyntaxTest.scala
@@ -9,6 +9,13 @@ object SyntaxTest extends BaseSpec {
 
   val spec: Spec[Environment, Any] =
     suite("SyntaxTest")(
+      test("<*> is an alias for zip") {
+        import zio.config.ConfigDescriptor.string
+
+        val _: ConfigDescriptor[(String, String, String)] = (string("A") <*> string("B") <*> string("C"))
+
+        assertCompletes
+      },
       test("config.narrow") {
         case class Cfg(a: String, b: Int)
         val cfg = ZLayer.succeed(Cfg("a", 1))
