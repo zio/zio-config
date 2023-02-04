bcbc10dd77ace0f7a95c678fedbf8212789e6c70
Afsl Thaj <afsal.taj06@gmail.com>
2023-02-05 04:08:58 +1100

    Add example for manual derivation for ADT using enumerate

---
+++ examples/shared/src/main/scala/zio/config/examples/AdtManualDerivation.scala
@@ -0,0 +1,26 @@
+package zio.config.examples
+
+import zio.config._
+import zio.{Config, ConfigProvider}
+
+object AdtManualDerivation extends App {
+  sealed trait FooBar
+
+  case class Foo(a: Int) extends FooBar
+
+  case class Bar(b: String) extends FooBar
+
+  val config =
+    Config.enumeration[FooBar](Config.int("a").to[Foo], Config.string("b").to[Bar])
+
+  private val source =
+    ConfigProvider.fromMap(
+      Map(
+        "a" -> "1"
+      )
+    )
+
+  //TODO; Not working for option
+  assert(source.load(config) equalM Foo(1))
+
+}
