04769028c5fad5ef1e139e56dfef5aa0decb4ed1
Afsl Thaj <afsal.taj06@gmail.com>
2023-03-06 09:49:50 +1100

    Update an example

--- examples/shared/src/main/scala/zio/config/examples/StandardPattern.scala
+++ examples/shared/src/main/scala/zio/config/examples/StandardPattern.scala
@@ -2,10 +2,9 @@ package zio.config.examples
 
 import zio.{Config, Runtime, ZIO, ZLayer}
 import zio.config.magnolia.deriveConfig
-import zio.config._
 import zio.config.typesafe.TypesafeConfigProvider
 
-object TestApp2 extends zio.ZIOAppDefault {
+object StandardPattern extends zio.ZIOAppDefault {
 
   val config = """arr = ["a", "b", "c"]"""
 
