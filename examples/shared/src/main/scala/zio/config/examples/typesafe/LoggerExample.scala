917f4d7db2c6e3cd314b9f7c9806111900355ae0
Afsal Thaj <afsal.taj06@gmail.com>
2023-03-07 04:20:54 +1100

    Add examples

--- examples/shared/src/main/scala/zio/config/examples/typesafe/LoggerExample.scala
+++ examples/shared/src/main/scala/zio/config/examples/typesafe/LoggerExample.scala
@@ -1,77 +1,23 @@
 package zio.config.examples.typesafe
 
-import zio.config.typesafe.TypesafeConfigProvider
-import zio.{Chunk, Config, LogLevel, Unsafe}
-import zio.config._
-import zio.config.examples.typesafe.LoggerExample.LoggingConfig.FilterConfig
+import zio.{ Config, ConfigProvider, Unsafe}
+import zio.config.typesafe._
+import zio.config.magnolia._
 
 object LoggerExample extends App {
 
-  final case class LoggingConfig(pattern: String, path: String, filter: FilterConfig)
-
-  object LoggingConfig {
-    val config: Config[LoggingConfig] =
-      Config
-        .string("pattern")
-        .zip(Config.string("log path"))
-        .zip(FilterConfig.config.nested("filter"))
-        .to[LoggingConfig]
-        .nested("logger")
-
-    final case class FilterConfig(rootLevel: String, mappings: Map[String, LogLevel])
-
-    object FilterConfig {
-      val config: Config[FilterConfig] =
-        Config
-          .string("rootLevel")
-          .zip(Config.table("mappings", Config.string.mapOrFail(logLevelValue)).withDefault(Map.empty))
-          .to[FilterConfig]
-
-    }
-
-  }
-  val logConfigJson =
+  val hocon =
     s"""
-       |logger {
-       |
-       |  pattern = "%timestamp{yyyy-MM-dd'T'HH:mm:ssZ} %level [%fiberId] %name:%line %message %cause"
-       |  "log path" = "file:///tmp/console_app.log"
-       |
-       |  filter {
-       |    rootLevel = "DEBUG"
-       |
-       |    mappings {
-       |      abc = "DEBUG"
-       |      "zio.logging.example.LivePingService" = "DEBUG"
-       |    }
-       |  }
-       |}
-       |
+       | duration = "10.seconds"
        |""".stripMargin
 
-  val logLevelMapping: Map[String, LogLevel] = Map(
-    LogLevel.All.label     -> LogLevel.All,
-    LogLevel.Trace.label   -> LogLevel.Trace,
-    LogLevel.Debug.label   -> LogLevel.Debug,
-    LogLevel.Info.label    -> LogLevel.Info,
-    LogLevel.Warning.label -> LogLevel.Warning,
-    LogLevel.Error.label   -> LogLevel.Error,
-    LogLevel.Fatal.label   -> LogLevel.Fatal,
-    LogLevel.None.label    -> LogLevel.None
-  )
-
-  def logLevelValue(value: String): Either[Config.Error.InvalidData, LogLevel] =
-    logLevelMapping.get(value.toUpperCase) match {
-      case Some(l) => Right(l)
-      case None    => Left(Config.Error.InvalidData(Chunk.empty, s"Expected a LogLevel, but found ${value}"))
-    }
-
-  val configProvider = TypesafeConfigProvider.fromHoconString(logConfigJson)
+  final case class 
+  val pgm2 = ConfigProvider.fromHoconString(hocon).load(deriveConfig[])
 
   println(Unsafe.unsafe { implicit u =>
     zio.Runtime.default.unsafe
       .run(
-        configProvider.load(LoggingConfig.config)
+        pgm2
       )
       .getOrThrowFiberFailure()
   })
