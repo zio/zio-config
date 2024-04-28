bbf41843d3321ed6f72e869f9f5b4095f78462f3
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-29 03:42:27 +1000

    Update SimpleReadConfigExample.scala

--- examples/shared/src/main/scala/zio/config/examples/SimpleReadConfigExample.scala
+++ examples/shared/src/main/scala/zio/config/examples/SimpleReadConfigExample.scala
@@ -3,8 +3,6 @@ package zio.config.examples
 import zio.config._
 import zio.{Config, ConfigProvider, Console, _}
 
-import scala.annotation.nowarn
-
 final case class Prod(ldap: String, port: Int, dburl: Option[String])
 
 object Prod {
@@ -20,7 +18,6 @@ object ReadConfig extends ZIOAppDefault {
   private val configProvider: ConfigProvider =
     ConfigProvider.fromMap(Map("LDAP" -> "ldap", "PORT" -> "1999", "DB_URL" -> "ddd"))
 
-  @nowarn("cat=lint-infer-any")
   def run: URIO[Any, ExitCode] =
     read(Prod.prodConfig from configProvider)
       .foldZIO[Any, Throwable, Any](
